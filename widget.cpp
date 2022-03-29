1ed8af86661ce32590c4e7b5b08c15e19d1cf519
David2574 <98658434+David2574@users.noreply.github.com>
2022-03-29 16:43:04 +0800

    Add files via upload

--- widget.cpp
+++ widget.cpp
@@ -2,26 +2,66 @@
 #include "ui_widget.h"
 #include <QImage>
 #include <QPixmap>
+#include <controller_dialog.h>
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    show_picture();
+    QImage *image1 = new QImage("C:/Users/jxrt/Desktop/GUI/QT_gui/QT_gui/logo.jpg");
+    ui->label_logo->setPixmap(QPixmap::fromImage(*image1));
+    ui->label_logo->show();
+    ui->textBrowser_software_intro->setHidden(true);
+    ui->textBrowser_team_intro->setHidden(true);
+    ui->pushButton_start->setStyleSheet("background-color: rgb(85, 170, 255);color:white");
+    ui->pushButton_software_into->setStyleSheet("background-color: rgb(85, 170, 255);color:white");
+    ui->pushButton_team_intro->setStyleSheet("background-color: rgb(85, 170, 255);color:white");
+    connect(ui->pushButton_start,SIGNAL(clicked(bool)),this,SLOT(pushbutton_start_clicked()));
+    connect(ui->pushButton_software_into,SIGNAL(clicked(bool)),SLOT(pushbutton_software_intro_clicked()));
+    connect(ui->pushButton_team_intro,SIGNAL(clicked(bool)),this,SLOT(pushbutton_team_intro_clicked()));
+
 }
 
 Widget::~Widget()
 {
     delete ui;
 }
-void Widget::show_picture()
+void Widget::pushbutton_start_clicked()
 {
-    QImage *image1 = new QImage("C:/Users/bbz/Desktop/GUI/control_gui/control_gui/control_gui/images/show.jpg");
-    ui->label_show->setPixmap(QPixmap::fromImage(*image1));
-    ui->label_show->show();
 
-    QImage *image2 = new QImage("C:/Users/bbz/Desktop/GUI/control_gui/control_gui/control_gui/images/guide.jpg");
-    ui->label_guide->setPixmap(QPixmap::fromImage(*image2));
-    ui->label_guide->show();
+    controller_dialog *controller_info = new controller_dialog();
+    controller_info->show();
+}
+
+void Widget::pushbutton_software_intro_clicked()
+{
+    QColor bgcolor(0,0,0);
+    bgcolor = ui->pushButton_software_into->palette().color(QPalette::Background);
+    if(bgcolor == QColor(85, 170, 255))
+    {
+        ui->textBrowser_software_intro->show();
+        ui->pushButton_software_into->setStyleSheet("background-color: rgb(0, 0, 0);color:white");
+    }
+    else
+    {
+        ui->textBrowser_software_intro->hide();
+        ui->pushButton_software_into->setStyleSheet("background-color: rgb(85, 170, 255);color:white");
+    }
+}
+
+void Widget::pushbutton_team_intro_clicked()
+{
+    QColor bgcolor(0,0,0);
+    bgcolor = ui->pushButton_team_intro->palette().color(QPalette::Background);
+    if(bgcolor == QColor(85, 170, 255))
+    {
+        ui->textBrowser_team_intro->show();
+        ui->pushButton_team_intro->setStyleSheet("background-color: rgb(0, 0, 0);color:white");
+    }
+    else
+    {
+        ui->textBrowser_team_intro->hide();
+        ui->pushButton_team_intro->setStyleSheet("background-color: rgb(85, 170, 255);color:white");
+    }
 
 }
